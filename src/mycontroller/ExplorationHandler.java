069e60b2e2e194bf05c01596de4c754d7702218b
Meriç Üngör <>
2019-10-24 22:30:53 +1100

    Implemented backing up from a hole. The logic for it is in the update method of the ExplorationHandler

--- src/mycontroller/ExplorationHandler.java
+++ src/mycontroller/ExplorationHandler.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import controller.CarController;
 import tiles.MapTile;
 import utilities.Coordinate;
+import world.WorldSpatial;
 import world.WorldSpatial.RelativeDirection;
 
 /**
@@ -19,10 +20,15 @@ public class ExplorationHandler {
 	private IExplorationStrategy exploreStrat;
 	private PathTracker pathTracker;
 	
+	private float health1UpdateBefore;
+	private boolean backingUp = false;
+	
 	public ExplorationHandler(CarController controller) {
 		this.controller = controller;
 		this.strategyFactory = StrategyFactory.getInstance();
 		this.pathTracker = new PathTracker();
+		
+		health1UpdateBefore = controller.getHealth();
 	}
 	
 	public void update() {
@@ -34,12 +40,24 @@ public class ExplorationHandler {
 		this.exploreStrat = strategyFactory.getExplorationStrategy(DEFAULT_EXPLORATION_STRATEGY, 
 				currentPos, view, controller.getOrientation(), pathTracker);
 		RelativeDirection dir = this.exploreStrat.getNextDirection();
-			
-		if(controller.getSpeed() < MyAutoController.CAR_MAX_SPEED) {
+		
+		
+		if(controller.getHealth() < health1UpdateBefore) {
+			health1UpdateBefore = controller.getHealth();
+			backingUp = true;
+		}
+
+		if(backingUp) {
+			//Back up until there is a wall behind you
+			if(checkBehind())
+				backingUp = false;
+			else
+				controller.applyReverseAcceleration();
+		}
+		else if(controller.getSpeed() < MyAutoController.CAR_MAX_SPEED) {
 			controller.applyForwardAcceleration();
 		}
-	
-		if(dir != null) {
+		else if(dir != null) {
 			if(dir == RelativeDirection.LEFT) {
 				controller.turnLeft();
 			}
@@ -52,4 +70,29 @@ public class ExplorationHandler {
 		pathTracker.update(new Coordinate(controller.getPosition()), PathTracker.VISITED_TILE_SCORE);
 		
 	}
+		
+	private boolean checkBehind() {
+		Coordinate carCoord = new Coordinate(controller.getPosition());
+		
+		if(controller.getOrientation() == WorldSpatial.Direction.NORTH) {
+			if(controller.getMap().get(new Coordinate(carCoord.x, carCoord.y-1)).getType().toString() == "WALL")
+				return true;
+		} else if(controller.getOrientation() == WorldSpatial.Direction.EAST) {
+			if(controller.getMap().get(new Coordinate(carCoord.x-1, carCoord.y)).getType().toString() == "WALL")
+				return true;
+		} else if(controller.getOrientation() == WorldSpatial.Direction.SOUTH) {
+			if(controller.getMap().get(new Coordinate(carCoord.x, carCoord.y+1)).getType().toString() == "WALL")
+				return true;
+		} else if(controller.getOrientation() == WorldSpatial.Direction.WEST) {
+			if(controller.getMap().get(new Coordinate(carCoord.x+1, carCoord.y)).getType().toString() == "WALL")
+				return true;
+		}
+		return false;
+	}
+	
+	
+	
+	
+	
+	
 }
